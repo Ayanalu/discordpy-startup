ee84ac622e8d384b8732a8b29994d6a00ff5d3e0
Ayanalu <57091366+Ayanalu@users.noreply.github.com>
2021-01-07 19:02:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -21,6 +22,35 @@ async def ping(ctx):
 @bot.command()
 async def neko(ctx):
     await ctx.send('にゃーん')
+    
+@bot.command()
+async def dice(message):
+    if message.author.bot:
+        return
+    if message.content.startswith("!dice"):
+        # 入力された内容を受け取る
+        say = message.content 
 
+        # [!dice ]部分を消し、AdBのdで区切ってリスト化する
+        order = say.strip('!dice ')
+        cnt, mx = list(map(int, order.split('d'))) # さいころの個数と面数
+        dice = diceroll(cnt, mx) # 和を計算する関数(後述)
+        await message.channel.send(dice[cnt])
+        del dice[cnt]
+
+        # さいころの目の総和の内訳を表示する
+        await message.channel.send(dice)
+
+def diceroll(cnt, max):
+    total = 0
+    num_list = []
+    for i in range(0, cnt):
+        # ランダムに1からサイコロの面数までの和を取得しリストに入れる
+        num = random.randint(1, max)
+        num_list.append(num)
+    # さいころの目の総和を計算しリストに入れる
+    total = sum(num_list)
+    num_list.append(total)
+    return num_list
 
 bot.run(token)
