1f5167eaa17b6c3ae75de19b064c96b906e9e99e
Ayanalu <57091366+Ayanalu@users.noreply.github.com>
2021-01-08 01:39:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,12 +22,11 @@ async def ping(ctx):
 @bot.command()
 async def neko(ctx):
     msg = await ctx.send('にゃーん')
-    await msg.add_reaction("\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}")
     
 @bot.command()
 async def vote(ctx, *, question):
     msg = await ctx.send(f'アンケート： {question}\n下の✔か☓で答えてください。')
-    await msg.add_reaction("\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}")
+    await msg.add_reaction("\N{WHITE HEAVY CHECK MARK (U+2705)}")
     
 @bot.command()
 async def roll(ctx, dice : str):
