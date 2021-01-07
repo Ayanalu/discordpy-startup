3dbd6aa6cbaa91a246179fcdc64e79281c7b08e8
Ayanalu <57091366+Ayanalu@users.noreply.github.com>
2021-01-07 19:59:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,13 +25,13 @@ async def neko(ctx):
     
 @bot.command()
 async def roll(ctx, dice : str):
-#     try:
-    rolls, limit = map(int, dice.split('d'))
-#     except Exception:
-#         await bot.say('Format has to be in NdN!')
-#         return
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
+    await ctx.send(result + "→ 合計")
                   
 bot.run(token)
