86c211975c86dd449e004c7b6a0e7385bb152723
Ayanalu <57091366+Ayanalu@users.noreply.github.com>
2021-01-08 15:55:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,7 +21,7 @@ async def on_reaction_add(reaction, user):
             dice = diceroll(1, 100)
             await reaction.message.channel.send(f'➨ **{dice[1]}**')
             del dice[1]
-            await reaction.message.channel.send(f'```内訳：1D100 {dice} {user.name}```')
+            await reaction.message.channel.send(f'```内訳：1D100 {dice} {user.mention}```')
         if reaction.emoji == "\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}":
             dice = diceroll(1, 10)
             await reaction.message.channel.send(f'➨ **{dice[1]}**')
