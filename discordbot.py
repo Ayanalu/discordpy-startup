af57b2b48ff26fae2d80b3ea44c234914e732f95
Ayanalu <57091366+Ayanalu@users.noreply.github.com>
2021-01-08 14:21:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,34 +19,34 @@ async def on_reaction_add(reaction, user):
     if reaction.count == 2:
         if reaction.emoji == "\N{DIGIT ZERO}\N{COMBINING ENCLOSING KEYCAP}":
             dice = diceroll(1, 100)
-            await reaction.message.channel.send(f'**{dice[1]}**')
+            await reaction.message.channel.send(f'```**{dice[1]}**```')
             del dice[1]
-            await reaction.message.channel.send(f'内訳：{dice}')
+            await reaction.message.channel.send(f'```内訳：{dice}```')
         if reaction.emoji == "\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}":
             dice = diceroll(1, 10)
-            await reaction.message.channel.send(f'**{dice[1]}**')
+            await reaction.message.channel.send(f'```**{dice[1]}**```')
             del dice[1]
-            await reaction.message.channel.send(f'内訳：{dice}')
+            await reaction.message.channel.send(f'```内訳：{dice}```')
         if reaction.emoji == "\N{DIGIT TWO}\N{COMBINING ENCLOSING KEYCAP}":
             dice = diceroll(3, 6)
-            await reaction.message.channel.send(f'**{dice[3]}**')
+            await reaction.message.channel.send(f'```**{dice[3]}**```')
             del dice[3]
-            await reaction.message.channel.send(f'内訳：{dice}')
+            await reaction.message.channel.send(f'```内訳：{dice}```')
         if reaction.emoji == "\N{DIGIT THREE}\N{COMBINING ENCLOSING KEYCAP}":
             dice = diceroll(1, 6)
-            await reaction.message.channel.send(f'**{dice[1]}**')
+            await reaction.message.channel.send(f'```**{dice[1]}**```')
             del dice[1]
-            await reaction.message.channel.send(f'内訳：{dice}')
+            await reaction.message.channel.send(f'```内訳：{dice}```')
         if reaction.emoji == "\N{DIGIT FOUR}\N{COMBINING ENCLOSING KEYCAP}":
             dice = diceroll(1, 4)
-            await reaction.message.channel.send(f'**{dice[1]}**')
+            await reaction.message.channel.send(f'```**{dice[1]}**```')
             del dice[1]
-            await reaction.message.channel.send(f'内訳：{dice}')  
+            await reaction.message.channel.send(f'```内訳：{dice}```')  
         if reaction.emoji == "\N{DIGIT FIVE}\N{COMBINING ENCLOSING KEYCAP}":
             dice = diceroll(2, 3)
-            await reaction.message.channel.send(f'**{dice[2]}**')
+            await reaction.message.channel.send(f'```**{dice[2]}**```')
             del dice[2]
-            await reaction.message.channel.send(f'内訳：{dice}')
+            await reaction.message.channel.send(f'```内訳：{dice}```')
             
             
 @bot.command()
@@ -60,7 +60,7 @@ async def neko(ctx):
 @bot.command()
 # async def dice(ctx, *, question):
 async def dice(ctx):
-    msg = await ctx.send(f'0：1D100\n1：1D10\n2：3D6\n3：1D6\n4：1D4\n5：2D3')
+    msg = await ctx.send(f'>>>0：1D100\n1：1D10\n2：3D6\n3：1D6\n4：1D4\n5：2D3')
     await msg.add_reaction("\N{DIGIT ZERO}\N{COMBINING ENCLOSING KEYCAP}")
     await msg.add_reaction("\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}")
     await msg.add_reaction("\N{DIGIT TWO}\N{COMBINING ENCLOSING KEYCAP}")
@@ -71,14 +71,16 @@ async def dice(ctx):
 
 @bot.command()
 async def roll(ctx, dice : str):
+    """NdNでダイスを振る"""
     rolls, limit = map(int, dice.split('d'))
     dice = diceroll(rolls, limit)
-    await ctx.send(f'**{dice[rolls]}**')
+    await ctx.send(f'```**{dice[rolls]}**```')
     del dice[rolls]
-    await ctx.send(f'内訳：{dice}')    
+    await ctx.send(f'```内訳：{dice}```')    
 
     
 def diceroll(rolls : int, limit : int):
+    """ダイスを振る(簡易)"""
     total = 0
     num_list = []
     for i in range(0, rolls):
